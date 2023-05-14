3a6b55274d831ea3a3b7087adaf17c91aa9e666c
min-sunnying <9909cindy@kaist.ac.kr>
2023-05-14 09:42:21 +0900

    student id call

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+const cors = require('cors');
+app.use(cors());
+
 //JSON read
 const rawData = fs.readFileSync('studentJSON.json');
 const data = JSON.parse(rawData);
@@ -11,4 +15,4 @@ app.get('/api/data', (req, res) => {
   res.json(data);
 });
 
-app.listen(5000, () => console.log('Server started on port 5000'));
+app.listen(8080, () => console.log('Server started on port 8080'));
--- src/App.js
+++ src/App.js
@@ -1,61 +1,75 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import { PeopleList, ChatHistory} from './chatroom';
 import axios from 'axios';
 
 function App() {
   const [data, setData] = useState([]);
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios('/api/data');
-      setData(result.data);
-    };
-    fetchData();
+    axios.get('http://localhost:8080/api/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }, []);
 
-  const [people, setPeople] = useState([
-    { id: 1, name: 'Alice' },
-    { id: 2, name: 'Bob' },
-    { id: 3, name: 'Charlie' },
-    { id: 4, name: 'Dave' },
-  ]);
-  const [selectedPerson, setSelectedPerson] = useState(people[0]);
-  const [messages, setMessages] = useState([
-    { id: 1, from: 1, to: 2, content: 'Hi Bob!', timestamp: '2022-05-07 10:00:00' },
-    { id: 2, from: 2, to: 1, content: 'Hi Alice!', timestamp: '2022-05-07 10:01:00' },
-    { id: 3, from: 3, to: 1, content: 'Hi Alice!', timestamp: '2022-05-07 10:02:00' },
-    { id: 4, from: 1, to: 3, content: 'Hi Charlie!', timestamp: '2022-05-07 10:03:00' },
-    { id: 5, from: 2, to: 3, content: 'Hi Charlie, how are you?', timestamp: '2022-05-07 10:04:00' },
-    { id: 6, from: 3, to: 2, content: 'I am good, thanks!', timestamp: '2022-05-07 10:05:00' },
-    { id: 7, from: 2, to: 3, content: 'What are you up to?', timestamp: '2022-05-07 10:06:00' },
-    { id: 8, from: 3, to: 2, content: 'Nothing much, just working on a project.', timestamp: '2022-05-07 10:07:00' },
-    ]);
+
+  // const [people, setPeople] = useState([
+  //   { id: 1, name: 'Alice' },
+  //   { id: 2, name: 'Bob' },
+  //   { id: 3, name: 'Charlie' },
+  //   { id: 4, name: 'Dave' },
+  // ]);
+  // const [selectedPerson, setSelectedPerson] = useState(people[0]);
+  // const [messages, setMessages] = useState([
+  //   { id: 1, from: 1, to: 2, content: 'Hi Bob!', timestamp: '2022-05-07 10:00:00' },
+  //   { id: 2, from: 2, to: 1, content: 'Hi Alice!', timestamp: '2022-05-07 10:01:00' },
+  //   { id: 3, from: 3, to: 1, content: 'Hi Alice!', timestamp: '2022-05-07 10:02:00' },
+  //   { id: 4, from: 1, to: 3, content: 'Hi Charlie!', timestamp: '2022-05-07 10:03:00' },
+  //   { id: 5, from: 2, to: 3, content: 'Hi Charlie, how are you?', timestamp: '2022-05-07 10:04:00' },
+  //   { id: 6, from: 3, to: 2, content: 'I am good, thanks!', timestamp: '2022-05-07 10:05:00' },
+  //   { id: 7, from: 2, to: 3, content: 'What are you up to?', timestamp: '2022-05-07 10:06:00' },
+  //   { id: 8, from: 3, to: 2, content: 'Nothing much, just working on a project.', timestamp: '2022-05-07 10:07:00' },
+  //   ]);
     
-    const handleSelectPerson = (person) => {
-    setSelectedPerson(person);
-    }
+  //   const handleSelectPerson = (person) => {
+  //   setSelectedPerson(person);
+  //   }
 
   //Show data
-  return (
-    <div className="container">
-      <div className="left-panel">
-        <h1>Student List</h1>
-        <PeopleList people={people} selectedPerson={selectedPerson} onSelectPerson={handleSelectPerson} />
-      </div>
-      <div className="right-panel">
-        <h1>Chatroom</h1>
-        <ChatHistory messages={messages} selectedPerson={selectedPerson} />
+  // return (
+  //   <div className="container">
+  //     <div className="left-panel">
+  //       <h1>Student List</h1>
+  //       <PeopleList people={people} selectedPerson={selectedPerson} onSelectPerson={handleSelectPerson} />
+  //     </div>
+  //     <div className="right-panel">
+  //       <h1>Chatroom</h1>
+  //       <ChatHistory messages={messages} selectedPerson={selectedPerson} />
+  //       <div>
+  //       <h1>Node.js에서 처리한 데이터</h1>
+  //       <ul>
+  //         {dataArray.map(item => (
+  //           <li key={item._id}>{item.studentID}</li>
+  //         ))}
+  //       </ul>
+  //       </div>
+  //     <div>
 
-      </div>
-      <div>
-      {data.map((item) => (
-        <div key={item.id}>
-          <h2>{item.title}</h2>
-          <p>{item.description}</p>
+  //   </div>
+  //   </div>
+  //   </div>
+  // );
+  return(
+        <div>
+        <h1>Node.js에서 처리한 데이터</h1>
+        <ul>
+          {data.map(item => (
+            <li key={item._id}>{item.studentID}</li>
+          ))}
+        </ul>
         </div>
-      ))}
-    </div>
-    </div>
   );
 }
 
